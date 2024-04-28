c4b88f5e11a853e37531c5d7f6512b722b16be19
Ajayprajapati0253 <167676141+Ajayprajapati0253@users.noreply.github.com>
2024-04-28 10:46:07 +0000

    datatype summery

---
+++ 01_basics/datatypesSummry.js
@@ -0,0 +1,39 @@
+// JavaScript is a dynamically typed language
+// primitive
+
+// 7->  string, boolean, number, null, undefined, symbol, bigInt
+
+const value = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+
+let userEmail 
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+const bigNumber = 2938233829389349849n
+
+
+
+// reference ( non primitive )
+//  Array, objects, functions
+
+// array
+const heros = ["shaktiman","doga","naagraj"];
+
+// objects
+let myObj = {
+    name :"Ajay",
+    age : 22,
+}
+
+// function
+const myFunction = function() {
+    console.log("hello doston");
+}
+
+console.log(typeof bigNumber)
