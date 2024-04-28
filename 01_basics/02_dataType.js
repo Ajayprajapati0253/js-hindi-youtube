4096f64dda2b8e246f50d826a3080460b3fc302b
Ajayprajapati0253 <167676141+Ajayprajapati0253@users.noreply.github.com>
2024-04-28 07:21:06 +0000

    datatypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,31 @@
+"use strict";    // treat all js code as newer version of 
+
+// alert(3+3)  // we are using nodejs not, browser
+
+console.log(3
+    +
+    3);   // code readability should be high
+
+console.log("Ajay")
+
+let name = "Ajay"
+let age = 22 
+let isLoggedIn = false
+
+let state ;   //  undefined
+
+// --- DATA TYPES-----
+// numbers => range 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value 
+// undefined
+// symbol => unique
+
+
+
+console.log(typeof age)
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
