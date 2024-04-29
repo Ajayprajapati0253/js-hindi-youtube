1708221e777d2a9c8edebb36fa3956a7799f6b13
Ajayprajapati0253 <167676141+Ajayprajapati0253@users.noreply.github.com>
2024-04-29 04:13:17 +0000

    number and maths

---
+++ 01_basics/06_number_and_Maths.js
@@ -0,0 +1,33 @@
+const score = 400
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// // console.log(balance.toString().length);
+// console.log(balance.toFixed(5));
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(5))
+
+
+// const hundres = 1000000
+// console.log(hundres.toLocaleString('en-IN'))
+
+
+//++++++++++++++ MATHS ++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-3))
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+
+console.log(Math.random());
+// Math.random() -> always give the value between 0 and 1
+console.log((Math.random()*10)+1)
+console.log(Math.floor((Math.random()*10)+1))
+
+// ++++++++ formula ++++++++
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
