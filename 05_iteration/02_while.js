669f5dfe3e3da423c2a6bf6e4e7f4267d8fce025
Ajayprajapati0253 <167676141+Ajayprajapati0253@users.noreply.github.com>
2024-04-30 05:17:29 +0000

    while loop in js

---
+++ 05_iteration/02_while.js
@@ -0,0 +1,25 @@
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ["ram" , "krishna","bholebaba"]
+let size = 0
+
+while(size < myArray.length){
+    // console.log(`Value is :-> ${myArray[size]}`);
+    size = size + 1;
+}
+
+
+// ++++++++++ do-while ++++++++++++++++
+
+// let score = 1;
+let score = 11;
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
