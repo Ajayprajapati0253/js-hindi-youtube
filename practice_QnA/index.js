f7ded07d638da8f49027b400b83e7a931c0c5a72
Ajayprajapati0253 <167676141+Ajayprajapati0253@users.noreply.github.com>
2024-07-23 05:51:55 +0000

    practice QnA

---
+++ practice_QnA/index.js
@@ -0,0 +1,36 @@
+// chapter 1 - Q1
+
+let a = "harry"
+let b = 6
+
+console.log(a+b)
+
+// chapter 1 - Q2
+
+console.log(typeof(b+a))
+
+
+// chapter 1 - Q3
+
+const obj1 = {
+    user : "Ajay",
+    isPrinciple : false,
+    section : 2
+}
+
+// chapter 1 - Q4
+obj1['age']= 45
+obj1['user']= "lovish"
+
+console.log(obj1)
+
+// chapter 1 - Q5
+
+const dict = {
+
+    appriciat:"good",
+    move:"turn in right path",
+    think:"in positive "
+}
+
+console.log(dict)
