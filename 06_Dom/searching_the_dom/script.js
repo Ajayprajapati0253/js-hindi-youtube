792b25787fd3baf23a5a2343daf00971a73a0e60
Ajayprajapati0253 <167676141+Ajayprajapati0253@users.noreply.github.com>
2024-07-28 17:32:58 +0000

    searching and table navigation

---
+++ 06_Dom/searching_the_dom/script.js
@@ -0,0 +1,14 @@
+// change the color of card-title
+
+// let ctitle = document.getElementsByClassName("card-title")[0];
+// ctitle.style.color = "red";
+
+let ctitles = document.querySelectorAll(".card-title");
+ctitles[0].style.color = "red";
+ctitles[1].style.color = "green";
+ctitles[2].style.color = "blue";
+console.log(ctitles);
+
+
+document.querySelector(".this").style.color = "cyan";
+document.querySelector(".this").style.backgroundColor = "black";
